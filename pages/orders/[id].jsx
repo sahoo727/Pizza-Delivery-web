0c10b6ae325c0736fe0499c6ff9094787d437070
sahoo727 <soubhagyasahoo727@gmail.com>
2022-10-28 16:18:54 +0530

    frontend

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,103 @@
+import styles from "../../styles/Order.module.css";
+import Image from "next/image";
+
+const Order = () => {
+    const status = 0;
+
+    const statusClass = (index) =>{
+        if (index - status < 1) return styles.done;
+        if (index - status === 1) return styles.inProgress;
+        if (index - status > 1) return styles.undone;
+    }
+
+  return (
+    <div className={styles.container}>
+
+        <div className={styles.left}>
+            <div className={styles.row}>
+                <table className={styles.table}>
+
+                <tr className={styles.tr}>
+                    <th>Order Id</th>
+                    <th>Customr</th>
+                    <th>Address</th>
+                    <th>Total</th>
+                </tr>
+
+                {/* 1st row */}
+                <tr className={styles.tr}>
+                    <td>
+                        <span className={styles.id}>12354384692629</span>
+                    </td>
+                    <td>
+                        <span className={styles.name}>
+                            John Doe
+                        </span>
+                    </td>
+                    <td>
+                        <span className={styles.address}>JP Nagar, 4h block, bangalore</span>
+                    </td>
+                    <td>
+                        <span className={styles.total}>39.80</span>
+                    </td>
+                </tr>
+
+                </table>
+            </div>
+
+            <div className={styles.row}>
+                <div className={statusClass(0)}>
+                    <Image src="/img/paid.png" width={30} height={30} alt="" style={{backgroundColor:"white"}}/>
+                    <span>Payment</span>
+                    <div className={styles.checkedIcon}>
+                     <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                    </div>
+                </div>
+
+                <div className={statusClass(1)}>
+                    <Image src="/img/bake.png" width={30} height={30} alt="" style={{backgroundColor:"white"}} />
+                    <span>Preparing</span>
+                    <div className={styles.checkedIcon}>
+                     <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                    </div>
+                </div>
+
+                <div className={statusClass(2)}>
+                    <Image src="/img/bike.png" width={30} height={30} alt="" style={{backgroundColor:"white"}} />
+                    <span>On The Way</span>
+                    <div className={styles.checkedIcon}>
+                     <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                    </div>
+                </div>
+
+                <div className={statusClass(3)}>
+                    <Image src="/img/delivered.png" width={30} height={30} alt="" style={{backgroundColor:"white"}} />
+                    <span>Delivered</span>
+                    <div className={styles.checkedIcon}>
+                     <Image src="/img/checked.png" width={20} height={20} alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className={styles.right}>
+            <div className={styles.wrapper}>
+                <h2 className={styles.title}>CART TOTAL</h2>
+                <div className={styles.totalText}>
+                    <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+                </div>
+                <div className={styles.totalText}>
+                    <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                </div>
+                <div className={styles.totalText}>
+                    <b className={styles.totalTextTitle}>Total:</b>$79.60
+                </div>
+                <button className={styles.button} disabled>Paid !!</button>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Order
